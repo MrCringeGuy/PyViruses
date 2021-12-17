008ea1b20638174bd69a5f0369f7d51b83b84c07
MrCringeGuy <93745239+MrCringeGuy@users.noreply.github.com>
2021-12-17 00:52:33 +0000

    Add files via upload

---
+++ Virus.py
@@ -0,0 +1,34 @@
+import pyautogui
+import time
+import random
+import pynput
+
+from pynput.keyboard import Key, Controller
+
+key = Controller()
+
+def roulette():
+    global random
+    global pyautogui
+    global pynput
+    numb = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+
+    if numb == 3:
+        while True:
+            ran1 = random.choice([0, 540, 1920, 740])
+            ran2 = random.choice([0, 234, 543, 1080])
+            print('Good luck with the mouse pointer.')
+            pyautogui.click(ran1, ran2)
+
+    elif numb == 7:
+        while True:
+            print('You pc is going to freeze soon.')
+            key.press('`')
+            key.release('`')
+
+    else:
+        print('You are safe for now...')
+
+while True:
+    roulette()
+    input('Press enter to roll again...')
